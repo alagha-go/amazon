fc184b4e638db56edab36d442e4680855e7e7ab3
alagha-go <abdihakimosman1480@gmail.com>
2022-04-29 16:33:28 +0000

    added mongodb connection

--- main.go
+++ main.go
@@ -2,18 +2,38 @@ package main
 
 import (
 	"amazon/lib/handler"
+	"amazon/lib/types"
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var (
 	port = ":6000"
+	mongodb = "mongodb://127.0.0.1:27017/"
 )
 
 func main() {
+	var err error
 	fmt.Println("starting server...")
+
+	clientOptions := options.Client().ApplyURI(mongodb)
+	ctx := context.Background()
+	types.Client, err = mongo.Connect(ctx, clientOptions)
+	HandleError(err)
 
 	go handler.Main()
 
 	http.ListenAndServe(port, nil)
+}
+
+
+func HandleError(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
 }
