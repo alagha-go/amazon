90b28eea25499e0ccf0acfd2de361f67dc0315fb
alagha-go <abdihakimosman1480@gmail.com>
2022-04-30 19:17:18 +0000

    added the missing https infront of the subcategory url

--- lib/crawler/subcategories.go
+++ lib/crawler/subcategories.go
@@ -21,16 +21,17 @@ func CollectSubCategories(category types.Category) []types.SubCategory {
 	collector := colly.NewCollector()
 	var SubCategories []types.SubCategory
 
+	
 	collector.OnHTML("#departments", func(element *colly.HTMLElement) {
 		element.ForEach(".a-spacing-micro.s-navigation-indent-2", func(_ int, element *colly.HTMLElement) {
 			var subCategory types.SubCategory
 			subCategory.ID = primitive.NewObjectID()
-			subCategory.Url = element.ChildAttr("a", "href")
+			subCategory.Url = "https://amazon.com" + element.ChildAttr("a", "href")
 			subCategory.Title = element.ChildText("a")
 			SubCategories = append(SubCategories, subCategory)
 		})
 	})
-
+	
 	collector.Visit(category.Url)
 	return SubCategories
 }
