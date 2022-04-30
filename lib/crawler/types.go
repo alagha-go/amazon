f17488f20bd6e379704e27c1877a04d5b54f9020
alagha-go <abdihakimosman1480@gmail.com>
2022-04-30 19:26:09 +0000

    get all types from each subcategory

---
+++ lib/crawler/types.go
@@ -0,0 +1,26 @@
+package crawler
+
+import (
+	"amazon/lib/types"
+
+	"github.com/gocolly/colly"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+
+func GetTypes(subcategory types.SubCategory) types.SubCategory {
+	collector := colly.NewCollector()
+
+	collector.OnHTML("#departments", func(element *colly.HTMLElement) {
+		element.ForEach(".a-spacing-micro.s-navigation-indent-2", func(_ int, element *colly.HTMLElement) {
+			var Type types.Type
+			Type.ID = primitive.NewObjectID()
+			Type.Url = "https://amazon.com" + element.ChildAttr("a", "href")
+			Type.Title = element.ChildText("a")
+			subcategory.Types = append(subcategory.Types, Type)
+		})
+	})
+	
+	collector.Visit(subcategory.Url)
+	return subcategory
+}
