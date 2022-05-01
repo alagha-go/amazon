5fa815ead9a980473340427779290c2dbc27d0cb
alagha-go <abdihakimosman1480@gmail.com>
2022-05-01 14:26:38 +0000

    tests subtypes.go

---
+++ lib/crawler/subtypes_test.go
@@ -0,0 +1,37 @@
+package crawler
+
+import (
+	"amazon/lib/types"
+	"encoding/json"
+	"io/ioutil"
+	"testing"
+)
+
+
+func TestGetSubTypes(t *testing.T) {
+	var passed bool
+	var departments []types.Department
+	data, err := ioutil.ReadFile("/home/ubuntu/Documents/amazon/testData/deps.json")
+	HandleError(err)
+	err = json.Unmarshal(data, &departments)
+	HandleError(err)
+
+	for dindex, department := range departments {
+		for cindex, category := range department.Categories {
+			for sindex, subcategory := range category.SubCategories {
+				for tindex, Type := range subcategory.Types {
+					departments[dindex].Categories[cindex].SubCategories[sindex].Types[tindex] = GetSubTypes(Type)
+					if len(departments[dindex].Categories[cindex].SubCategories[sindex].Types[tindex].SubTypes) > 0 {
+						passed = true
+					}
+				}
+			}
+		}
+	}
+
+	if !passed {
+		t.Error("failed to collect subcategories")
+	}
+	data = JsonMarshal(departments)
+	ioutil.WriteFile("/home/ubuntu/Documents/amazon/testData/deps.json", data, 0755)
+}
