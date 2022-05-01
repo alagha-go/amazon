2674c612466d01ab4c2e95ebd8a7d1b0d2fb54fa
alagha-go <abdihakimosman1480@gmail.com>
2022-05-01 14:14:53 +0000

    collects subtypes

---
+++ lib/crawler/subtypes.go
@@ -0,0 +1,26 @@
+package crawler
+
+import (
+	"amazon/lib/types"
+
+	"github.com/gocolly/colly"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+
+func GetSubTypes(Type types.Type) types.Type {
+	collector := colly.NewCollector()
+
+	collector.OnHTML("#departments", func(element *colly.HTMLElement) {
+		element.ForEach(".a-spacing-micro.s-navigation-indent-2", func(_ int, element *colly.HTMLElement) {
+			var subType types.SubType
+			subType.ID = primitive.NewObjectID()
+			subType.Url = "https://amazon.com" + element.ChildAttr("a", "href")
+			subType.Title = element.ChildText("a")
+			Type.SubTypes = append(Type.SubTypes, subType)
+		})
+	})
+	
+	collector.Visit(Type.Url)
+	return Type
+}
