e27c793692e4edaa27c0dac42e7a2fd12c1fe6df
alagha-go <abdihakimosman1480@gmail.com>
2022-05-01 19:00:14 +0000

    gets data from the database instead of the testing data

--- lib/handler/departments.go
+++ lib/handler/departments.go
@@ -1,15 +1,18 @@
 package handler
 
 import (
-	"io/ioutil"
+	"amazon/lib/departments"
 	"net/http"
 )
 
 
 func GetAllDepartments(res http.ResponseWriter, req *http.Request) {
-	data, err := ioutil.ReadFile("/home/ubuntu/Documents/amazon/testData/deps.json")
-	HandleError(err)
-	res.WriteHeader(200)
 	res.Header().Set("content-type", "application/json")
+	if req.Method != "GET" {
+		res.WriteHeader(400)
+		return
+	}
+	data, status := departments.GetDepartments()
+	res.WriteHeader(status)
 	res.Write(data)
 }
