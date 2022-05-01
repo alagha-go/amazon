8e05782781798b21546cfe13c4d28b1b14af4c96
alagha-go <abdihakimosman1480@gmail.com>
2022-05-01 18:40:05 +0000

    added error variables

--- lib/types/variable.go
+++ lib/types/variable.go
@@ -6,4 +6,12 @@ import (
 
 var (
 	Client *mongo.Client
+	CouldNotRetrieveData = Error{Error: "could not retrieve data from the database."}
+	CouldNotInsertData = Error{Error: "could not insert data to the database."}
+	UnAuthorizedAcces = Error{Error: "un Authorized Access."}
+	ExpiredToken = Error{Error: "expired Authorization token."}
+	InvalidApiKey = Error{Error: "invalid api key."}
+	PaymentExpired = Error{Error: "Your payment has expired. Update your payment to access this service."}
+	UserDoesNotExist = Error{Error: "User does not exist."}
+	DataDoesNotExist = Error{Error: "could not find the requested data."}
 )
