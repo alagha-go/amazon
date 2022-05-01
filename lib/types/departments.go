f24b17a0960bcb4b8b37f362436485dabfcd426e
alagha-go <abdihakimosman1480@gmail.com>
2022-05-01 18:05:53 +0000

    added some attributes to the types in favour of Product Type

--- lib/types/departments.go
+++ lib/types/departments.go
@@ -10,6 +10,7 @@ type Department struct {
 	Title							string											`json:"title,omitempty" bson:"title,omitempty"`
 	Url								string											`json:"url,omitempty" bson:"url,omitempty"`
 	Categories						[]Category										`json:"categories,omitempty" bson:"categories,omitempty"`
+	Category						*Category										`json:"category,omitempty" bson:"category,omitempty"`
 }
 
 type Category struct {
@@ -17,6 +18,7 @@ type Category struct {
 	Title							string											`json:"title,omitempty" bson:"title,omitempty"`
 	Url								string											`json:"url,omitempty" bson:"url,omitempty"`
 	SubCategories					[]SubCategory									`json:"subcategories,omitempty" bson:"subcategories,omitempty"`
+	SubCategory						*SubCategory									`json:"subcategory,omitempty" bson:"subcategory,omitempty"`
 }
 
 type SubCategory struct {
@@ -24,6 +26,7 @@ type SubCategory struct {
 	Title							string											`json:"title,omitempty" bson:"title,omitempty"`
 	Url								string											`json:"url,omitempty" bson:"url,omitempty"`
 	Types							[]Type											`json:"types,omitempty" bson:"types,omitempty"`
+	Type							*Type											`json:"type,omitempty" bson:"type,omitempty"`
 }
 
 type Type struct {
@@ -31,6 +34,7 @@ type Type struct {
 	Title							string											`json:"title,omitempty" bson:"title,omitempty"`
 	Url								string											`json:"url,omitempty" bson:"url,omitempty"`
 	SubTypes						[]SubType										`json:"subtypes,omitempty" bson:"subtypes,omitempty"`
+	SubType							*SubType										`json:"subtype,omitempty" bson:"subtype,omitempty"`
 }
 
 type SubType struct {
