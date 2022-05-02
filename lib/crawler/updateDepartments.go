8b7262a7bfa891bb2dfc6e93eb63245ebd0f23e3
alagha-go <abdihakimosman1480@gmail.com>
2022-05-02 08:34:10 +0000

    updates incomplete Departments

---
+++ lib/crawler/updateDepartments.go
@@ -0,0 +1,44 @@
+package crawler
+
+import (
+	"amazon/lib/departments"
+	"amazon/lib/types"
+	"encoding/json"
+)
+
+
+func UpdateDepartments() {
+	var Departments []types.Department
+	data, _ := departments.GetDepartments()
+	json.Unmarshal(data, &Departments)
+
+	if len(Departments) < 1 {
+		UpdateDepartments()
+	}
+
+	for _, Department := range Departments {
+		for cindex, Category := range Department.Categories {
+			if len(Category.SubCategories) >= 1 {
+				continue
+			}
+			Department.Categories[cindex].SubCategories = CollectSubCategories(Category)
+			Category.SubCategories = Department.Categories[cindex].SubCategories
+			for sindex, SubCategory := range Category.SubCategories {
+				if len(SubCategory.Types) >= 1 {
+					continue
+				}
+				Department.Categories[cindex].SubCategories[sindex] = GetTypes(SubCategory)
+				SubCategory = Department.Categories[cindex].SubCategories[sindex]
+				for tindex, Type := range SubCategory.Types {
+					if len(Type.SubTypes) >= 1 {
+						continue
+					}
+					Department.Categories[cindex].SubCategories[sindex].Types[tindex] = GetSubTypes(Type)
+					Type = Department.Categories[cindex].SubCategories[sindex].Types[tindex]
+				}
+			}
+		}
+
+		departments.UpdateDepartment(Department)
+	}
+}
