2b32acc471dcb7452ea4b4e2d922ca08ab268f16
alagha-go <abdihakimosman1480@gmail.com>
2022-06-21 07:04:42 +0000

    created structs fro departments

---
+++ departments/structs.go
@@ -0,0 +1,35 @@
+package departments
+
+
+type Department struct {
+	Title							string											`json:"title,omitempty" bson:"title,omitempty"`
+	Url								string											`json:"url,omitempty" bson:"url,omitempty"`
+	Categories						[]Category										`json:"categories,omitempty" bson:"categories,omitempty"`
+	Category						*Category										`json:"category,omitempty" bson:"category,omitempty"`
+}
+
+type Category struct {
+	Title							string											`json:"title,omitempty" bson:"title,omitempty"`
+	Url								string											`json:"url,omitempty" bson:"url,omitempty"`
+	SubCategories					[]SubCategory									`json:"subcategories,omitempty" bson:"subcategories,omitempty"`
+	SubCategory						*SubCategory									`json:"subcategory,omitempty" bson:"subcategory,omitempty"`
+}
+
+type SubCategory struct {
+	Title							string											`json:"title,omitempty" bson:"title,omitempty"`
+	Url								string											`json:"url,omitempty" bson:"url,omitempty"`
+	Types							[]Type											`json:"types,omitempty" bson:"types,omitempty"`
+	Type							*Type											`json:"type,omitempty" bson:"type,omitempty"`
+}
+
+type Type struct {
+	Title							string											`json:"title,omitempty" bson:"title,omitempty"`
+	Url								string											`json:"url,omitempty" bson:"url,omitempty"`
+	SubTypes						[]SubType										`json:"subtypes,omitempty" bson:"subtypes,omitempty"`
+	SubType							*SubType										`json:"subtype,omitempty" bson:"subtype,omitempty"`
+}
+
+type SubType struct {
+	Title							string											`json:"title,omitempty" bson:"title,omitempty"`
+	Url								string											`json:"url,omitempty" bson:"url,omitempty"`
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/alagha-go/go-amazon
 
 go 1.18
+
+require go.mongodb.org/mongo-driver v1.9.1
