4eb4ba3ecf9a45d70bcb8862a7eddd3581371f65
alagha-go <abdihakimosman1480@gmail.com>
2022-06-21 07:18:14 +0000

    test method to test function to get departments

---
+++ departments/1dep_test.go
@@ -0,0 +1,34 @@
+package departments
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"testing"
+)
+
+func TestDepartments(t *testing.T) {
+	deps := GetDepartments()
+	if len(deps) < 1 {
+		t.Error("error getting amazon departments")
+	}else {
+		data := JsonMarshal(deps)
+		err := ioutil.WriteFile("./DB/deps.json", data, 0755)
+		HandleError(err)
+	}
+}
+
+func HandleError(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func JsonMarshal(data interface{}) []byte {
+	var buff bytes.Buffer
+	encoder := json.NewEncoder(&buff)
+	encoder.SetEscapeHTML(false)
+	encoder.Encode(data)
+	return buff.Bytes()
+}
